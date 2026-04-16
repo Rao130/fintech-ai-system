787389cfbf45f1f4eaf0635ff494313d4cae6c1e
Rao130 <raosahab87023@gmail.com>
2026-04-16 07:50:12 +0530

    added history system

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import pickle
 import os
-
+history = []
 app = Flask(__name__)
 
 # Load model safely
@@ -23,7 +23,12 @@ def check_transaction():
     probability = model.predict_proba([[amount, time, location]])[0][1]
     result = "Fraud" if prediction == 1 else "Safe"
     risk_score = round(probability * 100, 2)
+    history.append({"amount": amount, "time": time, "location": location, "result": result, "risk_score": risk_score})
     return jsonify({"result": result, "risk_score": risk_score})
+
+@app.route('/history', methods=['GET'])
+def get_history():
+    return jsonify(history)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
